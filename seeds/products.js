cf1f6e41b9c3f53e7d78faa9533cb1f9d49bd663
Suphachai Hestmark <top_hestmark@yahoo.no>
2021-03-17 18:39:16 +0100

    Products image

---
+++ seeds/products.js
@@ -0,0 +1,41 @@
+export const products = [
+  {
+    id: 1,
+    name: "Fruit basket",
+    price: 39,
+    image: "./assets/products/product-1.jpg",
+  },
+  {
+    id: 2,
+    name: "Stick cup",
+    price: 14,
+    image: "./assets/products/product-2.jpg",
+  },
+  {
+    id: 3,
+    name: "Shell",
+    price: 7,
+    image: "./assets/products/product-3.jpg",
+  },
+];
+
+export const paints = [
+  {
+    id: 4,
+    name: "Paint 1",
+    price: 129,
+    image: "./assets/products/product-4.jpg",
+  },
+  {
+    id: 5,
+    name: "Paint 2",
+    price: 79,
+    image: "./assets/products/product-5.jpg",
+  },
+  {
+    id: 6,
+    name: "Paint 3",
+    price: 149,
+    image: "./assets/products/product-6.jpg",
+  },
+];
---
+++ src/products.js
@@ -0,0 +1,34 @@
+import { products, paints } from "../seeds/products.js";
+
+const productsContainer = document.querySelector(".products__container");
+const paintContainer = document.querySelector(".products__container.paint");
+
+products.map((product) => {
+  const id = product.id;
+  const name = product.name;
+  const image = product.image;
+  const price = product.price;
+
+  productsContainer.innerHTML += `
+    <div class="products__container__image-wrapper">
+      <img src=${image} alt=${name} />
+      <h4> ${name} </h4>
+      <p>$ ${price} </p>
+    </div>
+  `;
+});
+
+paints.map((product) => {
+  const id = product.id;
+  const name = product.name;
+  const image = product.image;
+  const price = product.price;
+
+  paintContainer.innerHTML += `
+    <div class="products__container__image-wrapper">
+      <img src=${image} alt=${name} />
+      <h4> ${name} </h4>
+      <p>$ ${price} </p>
+    </div>
+  `;
+});
